5ca632dd6845b37250904eaabc33cade1d3f1a5a
rifathosenofficial2023 <rifathosenofficial@gmail.com>
2025-11-11 20:41:27 +0600

    password textfield added

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -51,9 +51,30 @@ class _LoginPageState extends State<LoginPage> {
                   ),
                 ),
               ),
+              SizedBox(height: 10,),
 
           
               //password textfield
+               Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.grey[200],
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 20.0),
+                    child: TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: 'Password'
+                      ),
+                    ),
+                  ),
+                ),
+              ),
           
               //sign in button 
           
